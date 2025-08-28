b43090f4a74c261987f2e315361e6bf7f0c7c8fb
QuangThuan <tranquanthuan132@gmail.com>
2025-08-28 20:44:22 +0700

    update file hdhd

--- users/index.js
+++ users/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("User Service hello anh em");
+    res.send("User Service hello anh em balabala");
     });
 app.listen(5001, () => {
     console.log("🚀 User Service running at http://localhost:5001");
