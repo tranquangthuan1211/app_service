400a7620523517456196241807bed21d6425baf7
QuangThuan <tranquanthuan132@gmail.com>
2025-09-01 10:19:14 +0700

    hdgdg

--- orders/index.js
+++ orders/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("order Service dkjchcj");
+    res.send("order Service anh em hom nay là thu7");
     });
 app.listen(5003, () => {
-    console.log("🚀 User Service running at http://localhost:5003");
+    console.log("🚀 Order Service running at http://localhost:5003");
     });
