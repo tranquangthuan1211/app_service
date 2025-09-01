a9a4321374c1b57a8b87fd6a0d9f42e1b995e5dc
QuangThuan <tranquanthuan132@gmail.com>
2025-09-01 09:39:06 +0700

    update

--- products/index.js
+++ products/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("product service hhfh");
+    res.send("product service xin chao anh em");
     });
 app.listen(5002, () => {
     console.log("🚀 Product Service running at http://localhost:5002");
