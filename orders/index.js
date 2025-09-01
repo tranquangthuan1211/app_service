a2c6f2bd510ca1bda86fe9686ad1bfcb371f1b85
QuangThuan <tranquanthuan132@gmail.com>
2025-09-01 10:24:00 +0700

    hdgdg

--- orders/index.js
+++ orders/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("order Service anh em hom nay là thu7");
+    res.send("order Service anh em hom nay là thudnndnd7");
     });
 app.listen(5003, () => {
     console.log("🚀 Order Service running at http://localhost:5003");
