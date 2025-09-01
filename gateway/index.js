6374a1641355230c4174779258fa78a8680dfeb0
QuangThuan <tranquanthuan132@gmail.com>
2025-09-01 14:21:13 +0700

    update

--- gateway/index.js
+++ gateway/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Proxy tới User service
 app.get('/', (req, res) => {
-  res.send("hello api gatedndndnway");
+  res.send("hello api gateway");
 });
 app.use(
   "/users",
